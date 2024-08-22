7872c22003366d79b9bb6c8d0bc3c0e43d3d5ac4
João Lucas da Costa <63412716+LoopMon@users.noreply.github.com>
2024-08-21 21:19:03 -0300

    aula 21/08

--- routes/app.js
+++ routes/app.js
@@ -1,10 +1,41 @@
-var express = require('express'); 
+var express = require('express');
 var router = express.Router();
+var User = require('../models/user')
 
 router.get('/', (req, res, next) => {
     res.render('index');
 });
 
-module.exports = router; 
+router.get('/node-mongodb-mongoose-user', async (req, res, next) => {
+    try {
+        const userFind = await User.findOne({})
 
- 
+        res.render('node', {
+            firstNameV: userFind.firstName,
+            lastNameV: userFind.lastName,
+            passwordV: userFind.password,
+            emailV: userFind.email,
+            messagesV: userFind.messages
+        })
+    } catch (err) {
+        return res.send('Erro!!!')
+    }
+})
+
+router.post('/node-mongodb-mongoose-user', async (req, res, next) => {
+    var emailVar = req.body.emailBody
+    var userObject = new User({
+        firstName: 'Joao',
+        lastName: 'Lucas',
+        password: '1234',
+        email: emailVar
+    })
+
+    await userObject.save()
+
+    res.redirect('/node-mongodb-mongoose-user')
+})
+
+
+module.exports = router;
+
